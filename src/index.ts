7e20821e50cff7942042c72f7513f1f553766478
Zack Mulgrew <zack@zackthehuman.com>
2017-01-16 15:07:27 -0800

    Implement UI to partially allow for loading/stepping

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,74 @@
-console.log('It works!');
+const input = document.querySelector('input[type="file"]') as HTMLInputElement;
+const actionNextButton = document.querySelector('#action-next') as HTMLButtonElement;
+const worker = new Worker('worker/gameboy.js');
+
+const regUIForA = document.getElementById('register-A') as HTMLElement;
+const regUIForB = document.getElementById('register-B') as HTMLElement;
+const regUIForC = document.getElementById('register-C') as HTMLElement;
+const regUIForD = document.getElementById('register-D') as HTMLElement;
+const regUIForE = document.getElementById('register-E') as HTMLElement;
+const regUIForF = document.getElementById('register-F') as HTMLElement;
+const regUIForH = document.getElementById('register-H') as HTMLElement;
+const regUIForL = document.getElementById('register-L') as HTMLElement;
+const regUIForSP = document.getElementById('register-SP') as HTMLElement;
+const regUIForPC = document.getElementById('register-PC') as HTMLElement;
+
+const opcodeUI = document.getElementById('data-opcode') as HTMLElement;
+
+function formatByte(value: number): string {
+  const hex: string = value.toString(16);
+  return ('00' + hex).substring(hex.length);
+}
+
+function formatWord(value: number): string {
+  const hex: string = value.toString(16);
+  return ('0000' + hex).substring(hex.length);
+}
+
+function updateRegisterUI(values): void {
+  regUIForA.innerText = formatByte(values.A);
+  regUIForB.innerText = formatByte(values.B);
+  regUIForC.innerText = formatByte(values.C);
+  regUIForD.innerText = formatByte(values.D);
+  regUIForE.innerText = formatByte(values.E);
+  regUIForF.innerText = formatByte(values.F);
+  regUIForH.innerText = formatByte(values.H);
+  regUIForL.innerText = formatByte(values.L);
+  regUIForSP.innerText = formatWord(values.SP);
+  regUIForPC.innerText = formatWord(values.PC);
+}
+
+function updateOpcodeUI(opcode): void {
+  opcodeUI.innerText = formatByte(opcode);
+}
+
+input.addEventListener('change', function(this: HTMLInputElement, evt) {
+  var files = this.files;
+  if (files && files.length > 0) {
+    var load = {
+      cmd: 'load',
+      rom: files[0]
+    };
+
+    console.log('Loading ROM...');
+
+    worker.postMessage(load);
+  }
+}, false);
+
+actionNextButton.addEventListener('click', function(this: HTMLButtonElement, evt) {
+  const cycle = {
+    cmd: 'cycle'
+  };
+  worker.postMessage(cycle);
+});
+
+worker.onmessage = function(msg) {
+  switch (msg.data.cmd) {
+    case 'cycle':
+    console.log('Pong Cycle!', msg.data.registers);
+    updateRegisterUI(msg.data.registers);
+    updateOpcodeUI(msg.data.opcode);
+    break;
+  }
+};
--- src/worker/gameboy.ts
+++ src/worker/gameboy.ts
@@ -1,3 +1,40 @@
-import createOperations from './Operations';
+import createOperations from './operations';
+import createVirtualMachine from './vm';
 
 export default createOperations;
+
+const vm = createVirtualMachine();
+var ops = createOperations(vm);
+
+function readROMFile(file: File) {
+  const reader = new FileReaderSync();
+  const buffer = reader.readAsArrayBuffer(file);
+  vm.loadROM(new Uint8Array(buffer));
+}
+
+function cycle() {
+  const opcode = vm.pc.fetch();
+  vm.pc.increment();
+  ops.execOp(opcode);
+  postMessage({
+    cmd: 'cycle',
+    registers: vm.registers.toJSON(),
+    opcode: opcode.toByte()
+  });
+}
+
+onmessage = function(msg: MessageEvent) {
+  switch (msg.data.cmd) {
+    case 'load':
+      console.log('Reading ROM...');
+      readROMFile(msg.data.rom);
+      break;
+    case 'cycle':
+      cycle();
+      break;
+    case 'keydown':
+      break;
+    case 'keyup':
+      break;
+  }
+}
