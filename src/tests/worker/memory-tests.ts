10e62f8f80f699b9cf676d8af61621fa55f62a5b
Zack Mulgrew <zack@zackthehuman.com>
2017-01-20 16:49:21 -0800

    Enable memory mirroring for 0xC000 and 0xE000

--- src/tests/worker/memory-tests.ts
+++ src/tests/worker/memory-tests.ts
@@ -18,4 +18,34 @@ export default function memoryTests() {
     assert.equal(mem.readWord(0), 0x4002, 'word at 0 should be 0x4002');
     assert.equal(mem.readWord(2), 0xB33F, 'word at 2 should be 0xB33F');
   });
+
+  QUnit.test('internal RAM is echoed from 0xC000 to 0xE000', function(assert) {
+    const mem = new Memory();
+
+    mem.loadBytes([0x8, 0x6, 0x7, 0x5, 0x3, 0x0, 0x9], 0xC000);
+    assert.equal(mem.readByte(0xE000), 0x8, 'byte at 0xE000 should be 0x8');
+    assert.equal(mem.readByte(0xE001), 0x6, 'byte at 0xE001 should be 0x6');
+    assert.equal(mem.readByte(0xE002), 0x7, 'byte at 0xE002 should be 0x7');
+    assert.equal(mem.readByte(0xE003), 0x5, 'byte at 0xE003 should be 0x5');
+    assert.equal(mem.readByte(0xE004), 0x3, 'byte at 0xE004 should be 0x3');
+    assert.equal(mem.readByte(0xE005), 0x0, 'byte at 0xE005 should be 0x0');
+    assert.equal(mem.readByte(0xE006), 0x9, 'byte at 0xE006 should be 0x9');
+
+    mem.loadBytes([0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7], 0xE000);
+    assert.equal(mem.readByte(0xC000), 0x1, 'byte at 0xC000 should be 0x1');
+    assert.equal(mem.readByte(0xC001), 0x2, 'byte at 0xC001 should be 0x2');
+    assert.equal(mem.readByte(0xC002), 0x3, 'byte at 0xC002 should be 0x3');
+    assert.equal(mem.readByte(0xC003), 0x4, 'byte at 0xC003 should be 0x4');
+    assert.equal(mem.readByte(0xC004), 0x5, 'byte at 0xC004 should be 0x5');
+    assert.equal(mem.readByte(0xC005), 0x6, 'byte at 0xC005 should be 0x6');
+    assert.equal(mem.readByte(0xC006), 0x7, 'byte at 0xC006 should be 0x7');
+
+    const data: Array<number> = new Array<number>(7680);
+    data.fill(42);
+    mem.loadBytes(data, 0xC000);
+
+    for (let i = 0; i < data.length; ++i) {
+      assert.equal(mem.readByte(0xC000 + i), mem.readByte(0xE000 + i), 'byte at 0xC000 + i should equal byte at 0xE000 + i');
+    }
+  });
 }
--- src/worker/memory.ts
+++ src/worker/memory.ts
@@ -11,7 +11,11 @@ export class Memory {
 
   loadBytes(bytes: ArrayLike<number>, offset?: number): void;
   loadBytes(bytes: Uint8Array, offset?: number): void {
-    this.RAM.set(bytes, offset | 0);
+    const offsetAddress: number = offset || 0;
+
+    for (let i = 0; i < bytes.length; ++i) {
+      this.writeByte(offsetAddress + i, bytes[i]);
+    }
   }
 
   readByte(address: number): number {
@@ -23,11 +27,24 @@ export class Memory {
   }
 
   writeByte(address: number, value: number): void {
-    this.RAM[address] = value & 0xFF;
+    const byte: number = value & 0xFF;
+
+    // The addresses E000-FE00 appear to access the internal
+    // RAM the same as C000-DE00. (i.e. If you write a byte to
+    // address E000 it will appear at C000 and E000.
+    // Similarly, writing a byte to C000 will appear at C000
+    // and E000.)
+    if (address >= 0xE000 && address < 0xFE00) {
+      this.RAM[address] = this.RAM[0xC000 + (address - 0xE000)] = byte;
+    } else if (address >= 0xC000 && address < 0xDE00) {
+      this.RAM[address] = this.RAM[0xE000 + (address - 0xC000)] = byte;
+    } else {
+      this.RAM[address] = byte;
+    }
   }
 
   writeWord(address: number, value: number): void {
-    this.RAM[address] = (value & 0xFF00) >> 8;
-    this.RAM[address + 1] = value & 0xFF;
+    this.writeByte(address, (value & 0xFF00) >> 8);
+    this.writeByte(address + 1, value & 0xFF);
   }
 }
